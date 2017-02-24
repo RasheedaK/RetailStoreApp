3297e7db006067685866d960af80d83e56597cb3
krashee <krashee@thoughtworks.com>
2017-02-24 15:43:31 +0530

    Add Menu instance to main method and call showOptions

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
         RetailStoreApp retailStoreApp = dependency.initializeRetailStore();
         AppRunner appRunner = new AppRunner(retailStoreApp);
         Scanner sc = new Scanner(System.in);
+        Menu menu=dependency.setMenu();
         while (sc.hasNext()) {
+            menu.showOptions();
             appRunner.run(sc.nextInt());
         }
     }
